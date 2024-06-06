da563223187b29cf5ab3d17d41016a58a42329fd
Petro Podolets <petropodolets7@gmail.com>
2024-06-06 22:02:41 +0200

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -36,7 +36,12 @@ export const store = configureStore({
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware({
             serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+                ignoredActions: [FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER],
             },
         }),
 });
